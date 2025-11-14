0ec33b43068996193393f2245a6ae3fdb8aa0f78
Dural <Deniz.Dural@auma.com>
2025-11-14 12:26:19 +0100

    uhsjn

--- Übung/MainWindow.xaml.cs
+++ Übung/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace Übung
 {
@@ -16,89 +17,71 @@ namespace Übung
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int input_storage = 1;
+        private List<string> numbers = new List<string>();
+        
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        static int input_storage = 1;
-        string[] numbers = new string[input_storage];
+       private  void AddText(string value)
+        {
+            txt_down.Text += value;
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e) => AddText("0");
+        private void btn1_Click(object sender, RoutedEventArgs e) => AddText("1");
+        private void btn2_Click(object sender, RoutedEventArgs e) => AddText("2");
+        private void btn3_Click(object sender, RoutedEventArgs e) => AddText("3");
+        private void btn4_Click(object sender, RoutedEventArgs e) => AddText("4");
+        private void btn5_Click(object sender, RoutedEventArgs e) => AddText("5");
+        private void btn6_Click(object sender, RoutedEventArgs e) => AddText("6");
+        private void btn7_Click(object sender, RoutedEventArgs e) => AddText("7");
+        private void btn8_Click(object sender, RoutedEventArgs e) => AddText("8");
+        private void btn9_Click(object sender, RoutedEventArgs e) => AddText("9");
+
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void btn1_Click(object sender, RoutedEventArgs e)
-        {
-            txt_down.Text += "1";
-        }
-        private void btn2_Click(object sender, RoutedEventArgs e)
-        {
-            txt_down.Text += "2";
-        }
-        private void btn3_Click(object sender, RoutedEventArgs e)
-        {
-            txt_down.Text += "3";
-        }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
-        }
+        private void btnPlus_Click(object sender, RoutedEventArgs e) => AddText("+");
+        private void btnMinus_Click(object sender, RoutedEventArgs e) => AddText("-");
+        private void btnMutli_Click(object sender, RoutedEventArgs e) => AddText("*");
+        private void btnDiv_Click(object sender, RoutedEventArgs e) => AddText("/");
 
-        private void btn4_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
 
-        private void btn5_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
 
-        private void btn6_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
 
-        private void btnMinus_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
 
-        private void btn7_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
 
-        private void btn8_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-
-        private void btn9_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnPlus_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btn7_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void btnClear_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
 
         private void btnPass_Click(object sender, RoutedEventArgs e)
         {
-            
+            txt_down.Text = "";
+            try
+            {
+                string expr = txt_down.Text;
+                var result = new DataTable().Compute(expr, null);
+                txt_down.Text = result.ToString();
+            }
+            catch
+            {
+                txt_down.Text = "Fehler";
+            }
         }
+
+      
     }
 }
